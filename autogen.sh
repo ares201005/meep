b48e10a2989961512686aef13c89e4624088b62c
zhy <zhy@lanl.gov>
2022-09-11 15:44:53 -0600

    configure file cppflags

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,11 @@ autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 
-./configure --enable-maintainer-mode "$@"
+# add -L$LIBCTL_DIR/lib in the searching path
+
+sed -i 's@checking for vector3_dot in -lctlgeom@checking for vector3_dot in -L$LIBCTL_DIR/lib -lctlgeom@' configure
+sed -i 's@-lctlgeom  $LIBS@-L$LIBCTL_DIR/lib -lctlgeom  $LIBS@' configure
+sed -i 's@$CPPFLAGS $GUILE_CPPFLAGS@$CPPFLAGS $GUILE_CPPFLAGS -I$LIBCTL_DIR/include @' configure
+
+#./configure --without-scheme --with-libctl=/home/zhy/local/libctl/ "$@" 
+./configure --without-scheme --enable-maintainer-mode --with-libctl=/home/zhy/local/libctl/ --prefix=/home/zhy/Documents/polariton/meep_install CPPFLAGS="-I$HOME/local/libctl/include/"  "$@"
