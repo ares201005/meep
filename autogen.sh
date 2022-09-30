d3d700338eeb1b387a3db3b1fc1268cccf58cbc3
zhy <zhy@lanl.gov>
2022-09-30 00:04:36 -0600

    remove additional flags

--- autogen.sh
+++ autogen.sh
@@ -5,10 +5,6 @@ autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 autoreconf --verbose --install --symlink --force
 
-# add -L$LIBCTL_DIR/lib in the searching path
 
 ./configure --enable-maintainer-mode "$@"
 
-#sed -i 's@checking for vector3_dot in -lctlgeom@checking for vector3_dot in -L$LIBCTL_DIR/lib -lctlgeom@' configure
-#sed -i 's@-lctlgeom  $LIBS@-L$LIBCTL_DIR/lib -lctlgeom  $LIBS@' configure
-#sed -i 's@$CPPFLAGS $GUILE_CPPFLAGS@$CPPFLAGS $GUILE_CPPFLAGS -I$LIBCTL_DIR/include @' configure
